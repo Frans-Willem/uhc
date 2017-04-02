7df37717c113f1aec8b4c01d76456ade4800572c
Frans-Willem Hardijzer <fw@hardijzer.nl>
2017-04-02 19:30:18 +0200

    CPS: Fixed up unit testing script to compile CPSLib, and fixed up path to EHC.

--- CPS/UnitTesting/go.sh
+++ CPS/UnitTesting/go.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+CURDIR=$(pwd)
+cd ../cpslib
+./compile.sh
+cd $CURDIR
 ghc UnitTesting.hs
-MONO_PATH=../cpslib:$PATH ./UnitTesting ../uhc/EHC/install/8/bin/ehc ../samples
+MONO_PATH=../cpslib:$PATH ./UnitTesting ../../EHC/install/8/bin/ehc ../samples
